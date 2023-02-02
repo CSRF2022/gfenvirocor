d79f4e948d375879d7105fe83ef542010d7c2bec
ecophilina <paenglis@lakeheadu.ca>
2023-02-02 11:08:08 -0800

    clean up projection function

--- R/project-netcdf-values.R
+++ R/project-netcdf-values.R
@@ -1,16 +1,18 @@
 #' Get aggregated netcdf values for specific locations or grid cells
 #'
-#' @param rds.file File containing output from `extract-netcdf()` function
+#' @param rds.file File containing output from `extract_netcdf_values()` function
 #' @param variable_name What variable does the above file contain
-#' @param set_resolution Choose a resolution in m for the projected raster
-#' @param print_test_plot If `TRUE` will return a plot of how the resolution looks
-#' @param var_xvar_name What is the x variable from the rds called?
-#' @param var_yvar_name What is the Y variable from the rds called?
+#' @param set_resolution Choose a resolution in m for the projected raster#'
+#' @param radius Choose radius around points (only applies if not using a polygon). Default is half the resolution.
+#' @param print_test_plot If `TRUE` will return a plot of the first time step, showing how the resolution and extraction is working
+#' @param var_xvar_name What is the x variable from the rds called? Default matches output from `extract_netcdf_values()` function
+#' @param var_yvar_name What is the Y variable from the rds called? Default matches output from `extract_netcdf_values()` function
 #' @param var_crs What CRS projection was the netcdf file in?
-#' @param spatial_grid The grid to extract values for. Can be spatial object, or have x and y columns.
-#' @param grid_xvar_name If a spatial object, use `NULL`, otherwise give name of x variable in the grid.
-#' @param grid_yvar_name If a spatial object, use `NULL`, otherwise give name of y variable in the grid.
+#' @param coord_df Coordinates data frame. Can be spatial object, or any data frame with x and y columns
+#' @param grid_xvar_name Provide name of x variable in the grid, if not using a spatial object
+#' @param grid_yvar_name Provide name of y variable in the grid, if not using a spatial object
 #' @param grid_crs What CRS projection is the grid in?
+
 #'
 #' @return
 #' The grid expanded to include all years and the netcdf derived variables.
@@ -21,29 +23,31 @@
 #' project_netcdf_values(rds.file,
 #'   variable_name = "ann_mean",
 #'   print_test_plot = TRUE,
-#'   spatial_grid = bccoast,
+#'   coord_df = bccoast,
 #'   grid_xvar_name = "UTM.lon",
 #'   grid_yvar_name = "UTM.lat"
 #' )
 #'
-project_netcdf_values <- function(rds.file,
+project_netcdf_values <- function(nc.rds.file,
                                   variable_name,
                                   set_resolution = 10000,
+                                  radius = NULL,
                                   print_test_plot = FALSE,
                                   var_xvar_name = "X",
                                   var_yvar_name = "Y",
                                   var_crs = "+proj=longlat +datum=WGS84",
-                                  spatial_grid = bccoast,
+                                  coord_df = bccoast,
                                   grid_xvar_name = "UTM.lon",
                                   grid_yvar_name = "UTM.lat",
                                   grid_crs = "+proj=utm +zone=9 +datum=WGS84") {
-  spdf <- readRDS(rds.file) %>% mutate(X = .data[[var_xvar_name]], Y = .data[[var_yvar_name]])
 
-  coordinates(spdf) <- ~ X + Y
-  proj4string(spdf) <- CRS(var_crs)
+  spdf <- readRDS(nc.rds.file) %>% dplyr::mutate(X = .data[[var_xvar_name]], Y = .data[[var_yvar_name]])
 
-  # Create a base raster with with ROMS lat/lon extent
-  b <- rast(
+  sp::coordinates(spdf) <- ~ X + Y
+  sp::proj4string(spdf) <- sp::CRS(var_crs)
+
+  # Create a base raster ----------------------------------
+  b <- terra::rast(
     xmin = min(unique(spdf$X)), xmax = max(unique(spdf$X)),
     ymin = min(unique(spdf$Y)), ymax = max(unique(spdf$Y)),
     crs = var_crs
@@ -52,25 +56,25 @@ project_netcdf_values <- function(rds.file,
 
   # project
   r <- terra::project(b, grid_crs)
-  spdf <- spTransform(spdf, grid_crs)
+  spdf <- sp::spTransform(spdf, grid_crs)
 
-  # # set resolution to 3 km (the resolution of the ROMs grid), make this 4km so I have less NAs however this should be 3 km and interpolate data
-  res(r) <- set_resolution
+  # # set resolution
+  terra::res(r) <- set_resolution
   # plot(r)
 
   # Project values onto a 10km raster (values are a mean of surrounding cells)
   # xx <- rasterize(x=vect(spdf), y=r, field=names(spdf)[1], na.rm=F) # will loose a couple periperal cells
-  xx <- rasterize(x = vect(spdf), y = r, field = names(spdf)[1], na.rm = T)
+  xx <- terra::rasterize(x = terra::vect(spdf), y = r, field = names(spdf)[1], na.rm = T)
 
   # if(print_test_plot) raster::plot(xx)
   # raster::plot(xx)
   mx <- matrix(1, nc = 3, nr = 3)
-  # mx[2,1] <- NA  # will loose a couple periperal cells
+  # mx[2,1] <- NA  # will loose a couple peripheral cells
   xf <- terra::focal(xx, w = mx, fun = mean, na.policy = "only", na.rm = T)
   # raster::plot(xf)
 
   for (i in (2:length(names(spdf)))) {
-    xx <- rasterize(x = vect(spdf), y = r, field = names(spdf)[i], na.rm = T)
+    xx <- terra::rasterize(x = terra::vect(spdf), y = r, field = names(spdf)[i], na.rm = T)
     xf2 <- terra::focal(xx, w = mx, fun = mean, na.policy = "only", na.rm = T)
     xf <- c(xf, xf2)
   }
@@ -80,29 +84,94 @@ project_netcdf_values <- function(rds.file,
   # Overlay rasters with prediction points ----------------------------------
   r <- xf
 
-  # browser()
-  # grid <- spatial_grid
-  # spatial_grid$X <- spatial_grid[grid_xvar_name]
-  # spatial_grid$Y <- spatial_grid[grid_yvar_name]
+  if (is.null(radius)) {
+    radius <- set_resolution / 2
+  }
 
-  # this works for spatial points, should probably add buffer though and ultimately an option to use polygons
+  is_sf <- attr(coord_df, "sf_column")
 
-  if (!is.null(grid_xvar_name)) {
-    grid <- spatial_grid %>% mutate(X = .data[[grid_xvar_name]], Y = .data[[grid_yvar_name]])
+  if (!is.null(is_sf)) {
+    # there is a geometry ----------------------------------
+    what_geometry <- st_geometry_type(coord_df, by_geometry = FALSE)
+
+    if (what_geometry[1] == "POLYGON") {
+      # geometry is a polygon ----------------------------------
+      if (print_test_plot) {
+
+        ## test output
+        xy <- raster::extract(r[[1]], coord_df, weights = FALSE, fun = mean, na.rm = T)
+        df <- data.frame(cbind(z = xy[, 2], coord_df))
+        df <- sf::st_as_sf(df)
+        g <- ggplot(df) +
+          geom_sf(aes(colour = z)) +
+          scale_colour_viridis_c()
+      } else {
+
+        # coords to append to output
+        out <- coord_df
+
+        for (i in seq_along(1:length(names(spdf)))) {
+          xy <- raster::extract(r[[i]], coord_df, weights = FALSE, fun = mean, na.rm = T)
+          df <- data.frame(cbind(z = xy[, 2], coord_df))
+          colnames(df)[1] <- names(r[[i]])
+          out <- df %>%
+            dplyr::select(colnames(df)[1]) %>%
+            dplyr::bind_cols(out, .)
+          out
+        }
+        out
+      }
+    } else {
+      # geometry is not a polygon ----------------------------------
+      if (print_test_plot) {
+
+        ## test output
+        xy <- raster::extract(r[[1]], coord_df, buffer = radius, weights = FALSE, fun = mean, na.rm = T)
+        df <- data.frame(cbind(z = xy[, 2], coord_df))
+        df <- st_as_sf(df)
+        g <- ggplot(df) +
+          geom_sf(aes(colour = z)) +
+          scale_colour_viridis_c()
+      } else {
+
+        # coords to append to output
+        out <- coord_df
+
+        for (i in seq_along(1:length(names(spdf)))) {
+          xy <- raster::extract(r[[i]], coord_df, buffer = radius, weights = FALSE, fun = mean, na.rm = T)
+          df <- data.frame(cbind(z = xy[, 2], coord_df))
+          colnames(df)[1] <- names(r[[i]])
+          out <- df %>%
+            dplyr::select(colnames(df)[1]) %>%
+            dplyr::bind_cols(out, .)
+          out
+        }
+      }
+    }
+    if (!print_test_plot) {
+    # browser()
+    out2 <- tidyr::pivot_longer(out, ncol(coord_df):(ncol(out)-1),
+      names_to = "year", values_to = variable_name
+    )
+    out2
+    }else{
+      g
+    }
+  } else {
+    # no geometry at all ----------------------------------
+    grid <- coord_df %>% mutate(X = .data[[grid_xvar_name]], Y = .data[[grid_yvar_name]])
 
     # get just coordinates
     coords <- grid %>%
       dplyr::select(X, Y) %>%
-      distinct()
+      dplyr::distinct()
 
     if (print_test_plot) {
 
       ## test output
-      xy <- raster::extract(r[[1]], coords, weights = FALSE, fun = mean, na.rm = T)
+      xy <- raster::extract(r[[1]], coords, buffer = radius, weights = FALSE, fun = mean, na.rm = T)
       df <- data.frame(cbind(z = xy[, 2], coords))
-      # browser()
-
-      g <- ggplot2::ggplot(df) +
+      g <- ggplot(df) +
         geom_point(aes(X, Y, colour = z)) +
         scale_colour_viridis_c()
       g
@@ -112,46 +181,19 @@ project_netcdf_values <- function(rds.file,
       out <- coords
 
       for (i in seq_along(1:length(names(spdf)))) {
-        xy <- raster::extract(r[[i]], coords, weights = FALSE, fun = mean)
+        xy <- raster::extract(r[[i]], coords, buffer = radius, weights = FALSE, fun = mean, na.rm = T)
         df <- data.frame(cbind(z = xy[, 2], coords))
         colnames(df)[1] <- names(r[[i]])
         out <- df %>%
           dplyr::select(colnames(df)[1]) %>%
-          bind_cols(out, .)
+          dplyr::bind_cols(out, .)
         out
       }
-      out <- pivot_longer(out, 3:ncol(out), names_to = "year", values_to = variable_name)
-      out <- left_join(grid, out) %>% dplyr::select(-X, -Y)
-    }
-  } else {
-    if (print_test_plot) {
-
-      ## test output
-      xy <- raster::extract(r[[1]], spatial_grid, weights = FALSE, fun = mean, na.rm = T)
-      df <- data.frame(cbind(z = xy[, 2], spatial_grid))
-      df <- st_as_sf(df)
-      g <- ggplot2::ggplot(df) +
-        geom_sf(aes(colour = z)) +
-        scale_colour_viridis_c()
-      g
-    } else {
-
-      # coords to append to output
-      out <- spatial_grid
-
-      for (i in seq_along(1:length(names(spdf)))) {
-        xy <- raster::extract(r[[i]], spatial_grid, weights = FALSE, fun = mean)
-        df <- data.frame(cbind(z = xy[, 2], spatial_grid))
-        colnames(df)[1] <- names(r[[i]])
-        out <- df %>%
-          dplyr::select(colnames(df)[1]) %>%
-          bind_cols(out, .)
-        out
-      }
-      out <- pivot_longer(out, ncol(spatial_grid):ncol(out) - 1,
-        names_to = "year", values_to = variable_name
-      )
+      out <- tidyr::pivot_longer(out, 3:ncol(out), names_to = "year", values_to = variable_name)
+      out2 <- dplyr::left_join(grid, out) %>% dplyr::select(-X, -Y)
+      out2
     }
   }
-  out
+
+
 }
