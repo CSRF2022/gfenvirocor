61fed40f4a309f8b287866ad0e52150a0de77e22
ecophilina <paenglis@lakeheadu.ca>
2024-04-24 14:45:18 -0700

    species lists

--- analysis-condition/00-species-list.R
+++ analysis-condition/00-species-list.R
@@ -52,11 +52,55 @@ species_list <- list(
 # ## remove species?
 species_to_remove <- c(
   # "Slender Sole",
-  # "Curlfin Sole",
+  "Curlfin Sole",
   # "Sand Sole",
-  # "Harlequin Rockfish",
+  "Harlequin Rockfish",
   "Pacific Tomcod",
   "Pygmy Rockfish",
   "Rougheye/Blackspotted Rockfish Complex", #
   "Butter Sole" # samples only from HS and strange behaviour of RF
 )
+
+# might need additional species removed for imm?
+# e.g. Hake
+
+
+flatfish<- tolower(c(
+  "Curlfin Sole",#
+  "Butter Sole",
+  "Sand Sole",#
+  "Petrale Sole", #
+  "Arrowtooth Flounder", #
+  "English Sole",#
+  "Dover Sole",#
+  "Rex Sole", #
+  "Flathead Sole",#
+  "Southern Rock Sole",#
+  "Slender Sole",#
+  "Pacific Sanddab",#
+  "Pacific Halibut"#
+))
+
+rockfish<- tolower(c(
+  "Bocaccio",
+  "Canary Rockfish",
+  "Pacific Ocean Perch",
+  "Quillback Rockfish",
+  "Redbanded Rockfish",
+  "Redstripe Rockfish", #
+  "Rougheye/Blackspotted Rockfish Complex", #
+  "Silvergray Rockfish", #
+  "Shortspine Thornyhead",
+  "Widow Rockfish", #
+  "Yelloweye Rockfish",
+  "Yellowmouth Rockfish", #
+  "Yellowtail Rockfish",
+  ## "Copper Rockfish",
+  "Shortraker Rockfish",
+  "Rosethorn Rockfish",
+  "Harlequin Rockfish",
+  "Pygmy Rockfish",
+  "Sharpchin Rockfish",
+  "Darkblotched Rockfish",
+  "Greenstriped Rockfish"
+))
